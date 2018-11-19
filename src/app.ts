99782c2bf0392803dc92edd7e5e6a418c4fbaeda
samuraikun <tazitaziawawa@gmail.com>
2018-11-19 15:48:58 +0900

    finish sample donut chart

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
-import { Item } from "./item";
+import * as c3 from "c3";
 
-const elem = document.getElementById("output");
-const aBook = new Item("Hello TypeScript", 2980);
-aBook.say(elem);
+const chart = c3.generate({
+  bindto: "#chart",
+  data: {
+    type: "donut",
+    // tslint:disable-next-line:object-literal-sort-keys
+    columns: [["good", 50], ["bad", 50]]
+  },
+  donut: {
+    title: "Preact fast?"
+  }
+});
+
+setTimeout(() => {
+  chart.load({
+    columns: [["good", 30], ["bad", 30], ["best", 30]]
+  });
+}, 3000);
+
+setTimeout(() => {
+  chart.unload({
+    ids: "best"
+  });
+}, 5000);
